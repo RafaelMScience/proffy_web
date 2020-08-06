f94a57dc7f082497622d891b4fd686ed948d7bae
RafaelM <rafaelm.science@gmail.com>
2020-08-06 15:37:49 -0400

    project Web Porffy

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles.css";
+import PageHeader from "../../components/PageHeader";
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="que incrivel voce quer da aula" />
+    </div>
+  );
+}
+
+export default TeacherForm;
